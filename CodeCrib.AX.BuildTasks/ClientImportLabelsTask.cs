c8685cda971711d0892e5fa70d8ce4c7ea1b2ad6
Daniel Weichsel <daniel.weichsel@sikich.com>
2018-07-12 16:15:14 -0600

    More detail in label flush message

--- CodeCrib.AX.BuildTasks/ClientImportLabelsTask.cs
+++ CodeCrib.AX.BuildTasks/ClientImportLabelsTask.cs
@@ -98,7 +98,7 @@ namespace CodeCrib.AX.BuildTasks
                     AutoRunFile = Path.Combine(BuildPaths.Temp, string.Format(@"AutoRun_LabelFlush_{0}.xml", Guid.NewGuid()));
                     AxaptaAutoRun.SerializeAutoRun(AutoRun, AutoRunFile);
 
-                    BuildLogger.LogInformation(string.Format("Flushing imported label files"));
+                    BuildLogger.LogInformation(string.Format($"Flushing imported label file {labelFile} language {labelLanguage}"));
                     BuildLogger.StoreLogFile(AutoRunFile);
 
                     Client.Commands.AutoRun flushCommand = new Client.Commands.AutoRun() { ConfigurationFile = ConfigurationFile, Layer = Layer, LayerCode = LayerCode, Model = ModelName, ModelPublisher = Publisher, Filename = AutoRunFile };
